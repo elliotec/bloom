bd76fbe7019b6775620200addc0f7c80ed967c8d
elliotec <elliotecweb@gmail.com>
2016-02-11 19:20:06 -0700

    home, header, footer style changes including big header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202042057) do
+ActiveRecord::Schema.define(version: 20160204045941) do
 
   create_table "casein_admin_users", force: :cascade do |t|
     t.string   "login",                           null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20160202042057) do
     t.string   "time_zone"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "cleanses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "short_description"
+    t.string   "long_description"
+    t.decimal  "price"
+    t.string   "ingredients"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "juices", force: :cascade do |t|
