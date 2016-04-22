6152e0a4fe375d9c4c3d47405b4d8720d122deb4
elliotec <elliotecweb@gmail.com>
2016-04-21 21:03:51 -0600

    add proper url format to paperclip thing for s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,11 +48,13 @@ Rails.application.configure do
 config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
+      :url => 'bloomjuice.s3-us-west-2.amazonaws.com',
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-}
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      :path =>'/:class/:attachment/:id_partition/:style/:filename',
+      :s3_host_name => 's3-us-west-2.amazonaws.com'
+    }}
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
