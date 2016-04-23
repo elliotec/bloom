4a48de9462b6fad81aa9106b3417a79431ecaf88
elliotec <elliotecweb@gmail.com>
2016-04-23 14:50:31 -0600

    only enlarge smaller images instead of shrinking larger

--- app/models/elixer.rb
+++ app/models/elixer.rb
@@ -1,4 +1,4 @@
 class Elixer < ActiveRecord::Base
-  has_attached_file :photo, styles: {medium: "400x400>"}
+  has_attached_file :photo, styles: {medium: "800x800<"}
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
