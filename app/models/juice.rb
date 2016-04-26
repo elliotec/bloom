fb9fcbcbe1ac015fa76fdce020b0597390dbeaaf
elliotec <elliotecweb@gmail.com>
2016-04-26 00:57:47 -0600

    add next link on juices

--- app/models/juice.rb
+++ app/models/juice.rb
@@ -1,4 +1,13 @@
 class Juice < ActiveRecord::Base
   has_attached_file :photo, styles: {medium: "400x400<"}
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+
+  def previous
+    Juice.where(["id < ?", id]).last
+  end
+
+  def next
+    Juice.where(["id > ?", id]).first
+  end
 end
+
