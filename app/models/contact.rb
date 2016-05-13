981e5707263a99e46240920ea56e3f5e521b8c1a
elliotec <elliotecweb@gmail.com>
2016-05-13 10:53:32 -0600

    add maps link, subject line, fix elixer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,9 +2,10 @@ class Contact < MailForm::Base
   attribute :name, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message, validate: true
+  attribute :subject, validate: true
   def headers
     {
-      subject: "New contact form message from bloomjuiceco.com!",
+      subject: %("#{subject}"),
       to: "hello@bloomjuiceco.com",
       from: %("#{name}" <#{email}>)
     }
