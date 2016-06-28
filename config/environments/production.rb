d4970676586e1d845e8731cd907e745b498ad702
elliotec <elliotecweb@gmail.com>
2016-06-27 19:10:07 -0600

    add port 587

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     domain: "bloomjuiceco.com",
-    port: 465,
+    port: 587,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
